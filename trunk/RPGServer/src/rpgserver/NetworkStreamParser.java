aa6ebd69e0e4fe7a947544f04ba98a91718a2658
gauravnew <gauravnew@3e6ca13c-c4d7-11de-8613-b9ccef3c78c6>
2009-10-30 03:34:06 +0000

    NetworkStreamParser: Parses network packets recieved from the Client.

---
+++ trunk/RPGServer/src/rpgserver/NetworkStreamParser.java
@@ -0,0 +1,57 @@
+/*
+ * This class parses packets recieved from the client.
+ */
+
+package rpgserver;
+
+import java.io.*;
+
+/**
+ *
+ * @author gm
+ */
+public class NetworkStreamParser {
+
+    private BufferedReader netIn;
+
+    public NetworkStreamParser(BufferedReader in) {
+
+        netIn = in;
+        
+    }
+
+    public char [] getNextMessage() {
+
+        try {
+
+            while (!netIn.ready()) {}
+
+            char opcode[] = new char[2];
+            netIn.read(opcode,0,2);
+
+            int op = 255 * opcode[0] + opcode[1];
+
+            switch(op) {
+
+                case 'P'*255 + 'G':
+                    
+                    return opcode;
+
+                default:
+
+                    return null;
+                    
+            }
+
+        } catch (Exception e) {
+
+            System.out.println("Server Error.");
+
+            return null;
+
+        }
+        
+    }
+
+    
+}
