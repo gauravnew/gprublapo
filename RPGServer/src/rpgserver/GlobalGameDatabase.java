7813c65afd422e8ada12d0082a5382b652e50378
nvashkev <nvashkev@3e6ca13c-c4d7-11de-8613-b9ccef3c78c6>
2009-11-17 04:25:42 +0000

    Fixed the Bugs... now GlobalGameDatabase.java compiles

--- RPGServer/src/rpgserver/GlobalGameDatabase.java
+++ RPGServer/src/rpgserver/GlobalGameDatabase.java
@@ -63,7 +63,7 @@ public class GlobalGameDatabase {
     public synchronized boolean deleteActor(Integer ActorID) {
     	
     	//if the actor exists in the hashtable
-    	if(contains(ActorID)){
+    	if(DB.contains(ActorID)){
     		DB.remove(ActorID); //remove the key and the correspoding value(actor) from the hashtable
     		return true;
     	}
@@ -77,8 +77,7 @@ public class GlobalGameDatabase {
     //in the HashTable 'DB' which has the given ActorID.
     public synchronized Point2D getActorPosition(Integer ActorID) {
         
-    	Actor a = DB.get(ActorID); //get the actor with the ActorID  from the hashtable
-    	
+    	Actor a = (Actor) DB.get(ActorID);
     	return a.position; //return the actor's position
     }
 
@@ -87,9 +86,9 @@ public class GlobalGameDatabase {
     //in the HashTable 'DB' which has the given ActorID.
     public synchronized Point2D getActorMoveTo(Integer ActorID) {
     	
-    	Actor a = DB.get(ActorID); //get the actor with the ActorID  from the hashtable
+    	Actor a = (Actor) DB.get(ActorID); //get the actor with the ActorID  from the hashtable
 
-    	return a.moveTo;
+    	return a.moveto;
     }
 
     //TODO: Complete? (by Anastasia Vashkevich -- 11/15/09 6:30PM)
@@ -97,7 +96,7 @@ public class GlobalGameDatabase {
     //in the HashTable 'DB' which has the given ActorID.
     public synchronized float getActorSpeed(Integer ActorID) {
     	
-    	Actor a = DB.get(ActorID); //get the actor with the ActorID  from the hashtable
+    	Actor a = (Actor) DB.get(ActorID); //get the actor with the ActorID  from the hashtable
 
     	return a.speed;
     }
@@ -108,16 +107,16 @@ public class GlobalGameDatabase {
     //basically, it returns the 'status' variable of the
     //actor &(bitwise and) with the argument flag.
     public synchronized int getActorStatusFlags(Integer ActorID, int flag) {
-    	Actor a = DB.get(ActorID);
-        int stat = a.status
-    	return stat$flag;
+    	Actor a = (Actor) DB.get(ActorID);
+        int stat = a.status;
+    	return (stat & flag);
     }
 
     //TODO: Complete (by Anastasia Vashkevich -- 11/15/09 10PM)
     //This function returns the type of the actor
     //in the HashTable 'DB' which has the given ActorID.
     public synchronized int getActorType(Integer ActorID) {
-    	Actor a = DB.get(ActorID);
+    	Actor a = (Actor) DB.get(ActorID);
     	return a.type;
     }
 
@@ -125,7 +124,7 @@ public class GlobalGameDatabase {
     //This function sets the position of the actor
     //in the HashTable 'DB' which has the given ActorID.
     public synchronized void setActorPosition(Integer ActorID, Point2D p) {
-    	Actor a = DB.get(ActorID);
+    	Actor a = (Actor) DB.get(ActorID);
     	a.position = p;
     }
 
@@ -133,15 +132,15 @@ public class GlobalGameDatabase {
     //This function sets the moveto of the actor
     //in the HashTable 'DB' which has the given ActorID.
     public synchronized void setActorMoveTo(Integer ActorID, Point2D p) {
-    	Actor a = DB.get(ActorID);
-    	a.moveTo = p;
+    	Actor a = (Actor) DB.get(ActorID);
+    	a.moveto = p;
     }
 
     //TODO: Complete (by Anastasia Vashkevich -- 11/15/09 10PM)
     //This function sets the speed of the actor
     //in the HashTable 'DB' which has the given ActorID.
     public synchronized void setActorSpeed(Integer ActorID, float s) {
-    	Actor a = DB.get(ActorID);
+    	Actor a = (Actor) DB.get(ActorID);
     	a.speed = s;
     }
 
@@ -154,7 +153,7 @@ public class GlobalGameDatabase {
     //If the state is true then set the actor's 'status' to
     //status = status | flag; (bitwise or)
     public synchronized void setActorStatusFlag(Integer ActorID, int flag, boolean state) {
-    	Actor a = DB.get(ActorID);
+    	Actor a = (Actor) DB.get(ActorID);
     	if(state)
     		a.status = a.status|flag;
     	else
@@ -165,7 +164,7 @@ public class GlobalGameDatabase {
     //This function sets the type of the actor
     //in the HashTable 'DB' which has the given ActorID.
     public synchronized void setActorType(Integer ActorID, int type) {
-    	Actor a = DB.get(ActorID);
+    	Actor a = (Actor) DB.get(ActorID);
     	a.type = type;
     }
 
@@ -175,7 +174,7 @@ public class GlobalGameDatabase {
     //This is basically an array of all the 'keys' in the hashtable.
     public synchronized int [] getAllPlayerCharacters() {
     	Set keySet = DB.keySet();
-    	Object keyArray = keySet.toArray()
+    	Object keyArray = keySet.toArray();
     	return (int []) keyArray; 
     }
 
@@ -183,7 +182,7 @@ public class GlobalGameDatabase {
     //sets the name of the actor with the
     //given actorID.
     public synchronized void setActorName(Integer ActorID, String name) {
-    	Actor a = DB.get(ActorID);
+    	Actor a = (Actor) DB.get(ActorID);
     	a.name = name;
     }
     
