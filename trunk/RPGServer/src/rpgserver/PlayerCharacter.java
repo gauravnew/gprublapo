8a11c35617dbf78103960df780ce91e9115315dc
nvashkev <nvashkev@3e6ca13c-c4d7-11de-8613-b9ccef3c78c6>
2009-11-30 22:14:56 +0000

    Created a constructor for PlayerCharacter.java and added the datamembers from the Server Structure.txt

--- trunk/RPGServer/src/rpgserver/PlayerCharacter.java
+++ trunk/RPGServer/src/rpgserver/PlayerCharacter.java
@@ -23,11 +23,52 @@ package rpgserver;
  */
 public class PlayerCharacter extends Actor {
 
+	//datamembers
     ClientHandler client;
+	NetworkStreamWriter out;
+	
+	int[] classesAttended; //array that keeps track of what classes and how many times the classes were attended
+    
+	int health;
+	int lastClass; //last class that the player attended
+	int credits; //how many credits the player currently
+	
+	float distFromLastEx;
+    
+	boolean inBridge; //if the player is in the bridge circuit;
+	boolean sick; //if the player is sick (true if sick, false otherwise)
+
+	//constructor -- initializes the class datamembers
+	PlayerCharacter(){
+		this.type = 0;
+		
+		classesAttended = new int[9];
+		
+		health = 100;
+		lastClass = 0;
+		credits = 0;
+		
+		distFromLastEx = (float)0.0;
+		
+		inBridge = false;
+		sick = false;
+		
+	}
+	
+	Integer checkCollision(){
+		return null;
+		
+	}
+	
+	void processCollision(Integer id){
+		
+	}
 
     @Override
     public void updatePosition(Actor actor) {
         client.getNetworkOutput().sendActorMove(actor.actorID, actor.moveto);
     }
+    
+
 
 }
