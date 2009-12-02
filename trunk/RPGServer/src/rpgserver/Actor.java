7c8202432917c86d27a4ef48d58169d7a33939df
nvashkev <nvashkev@3e6ca13c-c4d7-11de-8613-b9ccef3c78c6>
2009-12-02 00:17:51 +0000

    created the updatePosition function.

--- trunk/RPGServer/src/rpgserver/Actor.java
+++ trunk/RPGServer/src/rpgserver/Actor.java
@@ -53,7 +53,21 @@ public class Actor implements Comparable {
     }
 
     public void updatePosition(Actor actor) {
+        //this function updates the actors current position based on moveTo position and time elapsed.
         
-    }
+    	//when was the last time the actor moved?
+    	if(actor.lastmovetime == 0){
+    		//if this is the first time the actor is moving, the actor will not move at this calculation -- will move next calc.
+    		actor.lastmovetime = System.nanoTime(); //if the actor didn't move, the last time it moved is NOW
+    	}
+    	
+    	//distance = time*speed
+    	long timeElapsed = actor.lastmovetime - System.nanoTime(); // how much time has passed between the last time actor moved and NOW
+    	
+    	float dist = timeElapsed*actor.speed; //distance
+    	actor.position.moveTo(dist, actor.moveto); //change actor position
+    	
+    	actor.lastmovetime = System.nanoTime(); //set the last time the actor moved to the system time.
+     }
     
 }
