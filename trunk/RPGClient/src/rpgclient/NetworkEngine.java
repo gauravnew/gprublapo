c601635b6fbd252528627c4b704447fa5edb4085
JAHampton123 <JAHampton123@3e6ca13c-c4d7-11de-8613-b9ccef3c78c6>
2009-12-10 03:18:23 +0000

    Commented out System.out.println statements

--- trunk/RPGClient/src/rpgclient/NetworkEngine.java
+++ trunk/RPGClient/src/rpgclient/NetworkEngine.java
@@ -31,7 +31,7 @@ public class NetworkEngine implements Runnable {
         return out;
     }
 
-	//Function to establish connection
+ //Function to establish connection
     private boolean connectToServer() {
 
         try {
@@ -57,51 +57,51 @@ public class NetworkEngine implements Runnable {
 
     @Override
     //Runnable thread
-	public void run() {
+ public void run() {
 
         if (!connectToServer()) return;
 
         try {
-			//Estabilsh 2 way network communication
+   //Estabilsh 2 way network communication
             out = new NetworkStreamWriter(new DataOutputStream(sckServer.getOutputStream()));
             in = new NetworkStreamParser(new DataInputStream(sckServer.getInputStream()));
         
             Main.coreLogic.setState(GAME_STATE.LOADING_STATE);
 
             Main.coreLogic.getLoadingScreen().incBy(20);
-			//[C.N.002]
+   //[C.N.002]
             out.sendLoginPacket(Main.getGameLogic().getMainActorName());
 
             while(true) {
-            	int id;
-            	Point2D target;
-            	movePkt pk;
+             int id;
+             Point2D target;
+             movePkt pk;
                 int opcode;
 
-				//read next message and process by opcode
+    //read next message and process by opcode
                 opcode = in.getNextMessageOPCode();
 
                 if (opcode != 0) {
-					//[C.N.011]
-					switch(opcode) {
+     //[C.N.011]
+     switch(opcode) {
                         //case ping
                         case 'P'*256 + 'G':
 
-                            System.out.println("NETWORK::Ping reply recieved.");
+                            // System.out.println("NETWORK::Ping reply recieved.");
 
                             break;
-						//case Map Image
+      //case Map Image
                         case 'M'*256 + 'I':
 
-                            System.out.println("NETWORK::Downloading Map Image.");
-                            in.getMapImage();		//[C.N.013]
+                            // System.out.println("NETWORK::Downloading Map Image.");
+                            in.getMapImage();  //[C.N.013]
                             Main.coreLogic.getLoadingScreen().incBy(50);
                             break;
-						//case Map Data
+      //case Map Data
                         case 'M'*256 + 'D':
 
-                            System.out.println("NETWORK::Downloading Map Data.");
-                            in.getMapData();		//[C.N.012]
+                            // System.out.println("NETWORK::Downloading Map Data.");
+                            in.getMapData();  //[C.N.012]
                             Main.coreLogic.getLoadingScreen().incBy(30);
                             Main.coreLogic.setState(GAME_STATE.INGAME_STATE);
                             Main.coreLogic.ui.setCountDown(30);
@@ -111,34 +111,34 @@ public class NetworkEngine implements Runnable {
                             int count = in.getCountDown();
                             Main.coreLogic.ui.setCountDown(count);
                             break;
-						//Case Move Actor
+      //Case Move Actor
                         case 'M'*256 + 'V':
                             pk = in.getActorMove();
                             id = pk.id.intValue();
                             target = pk.pos;
-                            System.out.println("NETWORK::Move Packet. " + id + " " + target.getX() + " " + target.getY());
+                            // System.out.println("NETWORK::Move Packet. " + id + " " + target.getX() + " " + target.getY());
                             try{Main.getGameLogic().getActorEngine().setActorMoveTo(id, target);}
                             catch (Exception e){System.out.println(e);}
                             break;
                         //Case New Actor Data
                         case 'N'*256 + 'A':
-                        	Main.getGameLogic().getActorEngine().addActor(in.getNewActorData());
+                         Main.getGameLogic().getActorEngine().addActor(in.getNewActorData());
                             break;
                         case 'M'*256 + 'G':
-                        	Main.getGameLogic().setMessage(in.getMessage());
+                         Main.getGameLogic().setMessage(in.getMessage());
                             break;
                         case 'L'*256 + 'C':
-                        	Main.getGameLogic().setLastClass(in.getLastClass());
-                        	break;
+                         Main.getGameLogic().setLastClass(in.getLastClass());
+                         break;
                         case 'G'*256 + 'O':
-                        	Main.getGameLogic().state=GAME_STATE.LOGIN_STATE;
-                        	Main.getGameLogic().setWinner(in.getGameOver());
-                        	break;
+                         Main.getGameLogic().state=GAME_STATE.LOGIN_STATE;
+                         Main.getGameLogic().setWinner(in.getGameOver());
+                         break;
                         case 'T'*256 + 'L':
                             pk = in.getActorMove();
                             id = pk.id.intValue();
                             target = pk.pos;
-                            System.out.println("NETWORK::Move Packet. " + id + " " + target.getX() + " " + target.getY());
+                            // System.out.println("NETWORK::Move Packet. " + id + " " + target.getX() + " " + target.getY());
                             try{Main.getGameLogic().getActorEngine().setActorPosition(id, target);}
                             catch (Exception e){System.out.println(e);}
                             break;
@@ -146,7 +146,7 @@ public class NetworkEngine implements Runnable {
 
                             //Unknown packet, disconnect and return.
                             sckServer.close();
-                            System.out.println("Server connection closed.");
+                            // System.out.println("Server connection closed.");
                             Main.coreLogic.setState(GAME_STATE.LOGIN_STATE);
                             return;
                     }
--- trunk/RPGServer/src/rpgserver/NPCEngine.java
+++ trunk/RPGServer/src/rpgserver/NPCEngine.java
@@ -16,18 +16,18 @@ import rpgserver.NonPlayerCharacter.RELATIVE_DIRECTION;
 
 /**
  * TODO
- *	implement generateNewPosition(Integer id) which both generates and sets
- *		a new moveto position for the given id based on direction.
+ * implement generateNewPosition(Integer id) which both generates and sets
+ *  a new moveto position for the given id based on direction.
  * @author Jacob
  */
 public class NPCEngine {
     
-	public NPCEngine() {};
+ public NPCEngine() {};
     
     //Reads each line individually from a file listing NPCs of the format
     //type:x:y:Name, where type, x, y are all ints, 3 digits, and name is optional.
     //(completed by Jacob Hampton, 11/17, at 12:15 AM)
-	//(modified by Lincoln Waller 12/4
+ //(modified by Lincoln Waller 12/4
     public void loadNPCsFromFile(String filename, GlobalGameDatabase db) {
         
         int type = 0000;
@@ -46,44 +46,44 @@ public class NPCEngine {
             int actorID = db.createNewNonPlayerCharacter(type, new Point2D(x,y));
             db.setActorPosition(actorID, position);
             if(str.length() > 11) {
-            	name = str.substring(12);
-            	db.setActorName(actorID, name);
+             name = str.substring(12);
+             db.setActorName(actorID, name);
             }
-            System.out.println("loaded character: " + type + " at " + x + " " + y);
+            //System.out.println("loaded character: " + type + " at " + x + " " + y);
         }
         in.close();
         } catch (IOException e) {
-        	System.out.println("NPC Load error: " + e);
+         System.out.println("NPC Load error: " + e);
         }
         
     }
     
     public void generateRandomCharacters(GlobalGameDatabase db) {
-    	//10 manhole, 10 prof, 5 H1N1
-    	int dir = 1;
-    	for(int i=0;i<10;i++) 
-    		{db.createNewNonPlayerCharacter(1, dir); dir*=-1;}
-    	for(int i=0;i<5;i++) 
-    		{db.createNewNonPlayerCharacter(2, dir); dir*=-1;}
-    	for(int i=0;i<10;i++) 
-    		{db.createNewNonPlayerCharacter(3, dir); dir*=-1;}
-    	
+     //10 manhole, 10 prof, 5 H1N1
+     int dir = 1;
+     for(int i=0;i<10;i++) 
+      {db.createNewNonPlayerCharacter(1, dir); dir*=-1;}
+     for(int i=0;i<5;i++) 
+      {db.createNewNonPlayerCharacter(2, dir); dir*=-1;}
+     for(int i=0;i<10;i++) 
+      {db.createNewNonPlayerCharacter(3, dir); dir*=-1;}
+     
     }
     
     public void generateNewPosition(Integer id) {
-    	GlobalGameDatabase db = Main.cGameLogic.getGameDB();
-    	GameMap map = Main.cGameLogic.getGameMap();
-    	NonPlayerCharacter moveMe = db.getNonPlayer(id);
-    	Point2D newTarget;
-    	if(moveMe.nextDirection==RELATIVE_DIRECTION.HORIZONTAL){
-    		newTarget = map.getRandomHorizontal(moveMe.position);
-    		moveMe.nextDirection=RELATIVE_DIRECTION.VERTICAL;
-    	}
-    	else{
-			newTarget = map.getRandomVertical(moveMe.position);
-			moveMe.nextDirection=RELATIVE_DIRECTION.HORIZONTAL;
-    	}
-    	db.setActorMoveTo(id, newTarget);
+     GlobalGameDatabase db = Main.cGameLogic.getGameDB();
+     GameMap map = Main.cGameLogic.getGameMap();
+     NonPlayerCharacter moveMe = db.getNonPlayer(id);
+     Point2D newTarget;
+     if(moveMe.nextDirection==RELATIVE_DIRECTION.HORIZONTAL){
+      newTarget = map.getRandomHorizontal(moveMe.position);
+      moveMe.nextDirection=RELATIVE_DIRECTION.VERTICAL;
+     }
+     else{
+   newTarget = map.getRandomVertical(moveMe.position);
+   moveMe.nextDirection=RELATIVE_DIRECTION.HORIZONTAL;
+     }
+     db.setActorMoveTo(id, newTarget);
     }
     
 }
